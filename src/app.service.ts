fb16b428f46f5bd214f984309b9545b43d3afb56
marina <mari68253@gmail.com>
2025-05-30 19:24:32 -0300

    actualizando

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@nestjs/common';
 
+export interface iTrack {
+  id: number,
+  title: string,
+  duracion: number,
+  artist: string,
+ }
+
+export const tracks: iTrack[] = [ 
+{
+  id: 1,
+  title: "cancion 2",
+  duracion: 120,
+  artist: "interprete 1",
+ },
+ {
+  id: 2,
+  title: "cancion 2",
+  duracion: 100,
+  artist: "interprete 2",
+ },
+ {
+  id: 3,
+  title: "cancion 3",
+  duracion: 110,
+  artist: "interprete 3",
+ }
+]
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getTracks():iTrack[] {
+    return tracks;
   }
 }
